a62737d560fbcf58bae65177820a0751670b56cb
Rascawl <velosoryan73@gmail.com>
2025-10-05 15:43:00 +0800

    Add basic task management menu and add feature
    Introduced a simple menu for adding, deleting, and editing tasks. Implemented the 'add task' functionality, allowing users to input and store tasks in a list. Added comments for future improvements and placeholders for delete and edit features.

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
-print("Hello world!")
+#task ay yung array natin na pag lalagyan ng mga task, hindi ko alam ano pinaka effective na way pero I think tasks.append() yung gagamitin natin para mag delete
+task = []
 
-Hello World("Print")
+#Ito menu natin gamit tayo def(), pero isip kapa din if may mas effective way mag handle ng options
+menu = input("""Hello user, what do you wanna do?
+                [A] Add a task
+                [D] Delete a task
+                [E] Edit a task
+                Enter the letter: """)
+
+#as of now di ko alam pano iloop program para makapag lagay ng multiple tasks... btw yung .lower() means tinatransform ko yung nakuhang string sa menu bilang lower case
+if menu.lower() == 'a':
+    add = input("Enter a task: ") 
+    task.append(add)
+
+    print("Here is your pending task: \n", ", ".join(task))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#scratch paper ko lang muna haha 
+#add = input("Enter your task: ")
+#tasks.append(add)
+#print("Here is your pending task: \n", ", ".join(tasks))
