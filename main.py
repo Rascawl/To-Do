231f0e1ef84f8d6f9b0545c7337d8104c919c047
Rascawl <velosoryan73@gmail.com>
2025-10-18 20:35:24 +0800

    Add save and read task functionality
    Introduced functions to save tasks to a file and read tasks from a file. Updated the addTasks function to prompt for saving tasks after addition. Added a sample tasks.txt file.

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
+
 #tasks ay yung array natin na pag lalagyan ng mga task, hindi ko alam ano pinaka effective na way pero I think tasks.append() yung gagamitin natin para mag delete
 tasks = []
+
+def saveTask():
+    file_path = input("Please enter the file name of your task: ")
+    try:
+        with open(file_path, "x") as file:
+            file.write("______THIS IS YOUR TASKS______\n")
+            for i, task in enumerate(tasks, start=1):
+                file.write(f"{i}. {task}\n")
+        print("Tasks saved")
+    except FileExistsError:
+        print("There is a list with the same name")
+
+def readTask():
+    file_path = input("Please enter the file name of the task you want to edit: ")
+    with open(file_path, "r") as file:
+        contentList = file.read()
+        print(contentList)
+
+
+def addTasks():
+    while True:
+        add = input("Enter a task (press enter to stop): ")  
+        if add.lower() == "":
+            break
+        else: 
+            tasks.append(add)
+            
+    print("______THIS IS YOUR TASKS______")
+    for i, add in enumerate(tasks, start =1):
+        print(f"{i}. {add}")
+
+    confirm = input("Do you want to save? Y/N: ")
+    if confirm.lower() == 'y':
+        saveTask()
 
 menu = input("""Hello user, whata do you wanna do?
                 [A] Add a task
@@ -7,25 +42,11 @@ menu = input("""Hello user, whata do you wanna do?
                 [E] Edit a task
                 Enter the letter: """)
 
-#mga def natin
-def addTasks():
-    while True:
-        add = input("Enter a task (press enter to stop): ")  #kailangan enter para ma input, pede mo ibahin to if u want
-        if add.lower() == "":
-            break
-        else: 
-            tasks.append(add)
-
-    print("______THIS IS YOUR TASKS______")
-    for add in tasks: #prints the tasks 
-        
-        print(add) #if kaya mo pre try mo lagyan ng number yung list natin like 1. 2. gamit ka loop idk din eh
-        
-
-  
-#as of now di ko alam pano iloop program para makapag lagay ng multiple tasks... btw yung .lower() means tinatransform ko yung nakuhang string sa menu bilang lower case
 if menu.lower() == 'a':
     addTasks()
+elif menu.lower() == 'e':
+    readTask()
+    
 
 
 
